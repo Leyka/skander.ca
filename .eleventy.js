87deb9e8147eeefd5a8e085ee1460714247f265a
Skander <skander.kc@gmail.com>
2022-11-27 17:16:30 +0000

    feat: sass to css

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,28 @@
+const esbuild = require('esbuild');
+const sass = require('esbuild-sass-plugin');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
+const filters = require('./libs/eleventy/filters');
 
-const filters = require('./utils/filters.utils');
+const isProdEnv = process.env.ELEVENTY_ENV === 'production';
 
 module.exports = function (config) {
   // Plugins
   config.addPlugin(pluginRss);
-
-  // Pass through files
-  config.addPassthroughCopy('./src/style.css');
-
   // Filters
   Object.keys(filters).forEach((filterName) => {
     config.addFilter(filterName, filters[filterName]);
   });
+  // Esbuild: Sass => CSS
+  config.on('afterBuild', () => {
+    return esbuild.build({
+      entryPoints: ['src/assets/sass/app.scss'],
+      outdir: 'dist/assets/css',
+      minify: isProdEnv,
+      sourcemap: isProdEnv,
+      plugins: [sass.sassPlugin()],
+    });
+  });
+  config.addWatchTarget('./src/assets/sass');
 
   return {
     dir: {
